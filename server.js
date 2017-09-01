cf014ffcc10e928c2814c11c9e9d3511aa1729e6
powi442 <k08ip01@gmail.com>
2017-09-01 09:30:13 -0700

    maybe?

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./app/routing/htmlroutes")(app);
-require("./app/routing/apiroutes")(app);
+require("./app/Routing/htmlroutes.js")(app);
+require("./app/Routing/apiroutes.js")(app);
 
 
 // ==============================================================================
