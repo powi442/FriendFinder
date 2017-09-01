444b8df8b15cbd763869e9fd5049895e6babf182
powi442 <k08ip01@gmail.com>
2017-09-01 09:32:45 -0700

    maybe-er

--- app/Routing/apiroutes.js
+++ app/Routing/apiroutes.js
@@ -3,7 +3,7 @@
 // We are linking our routes to our friend data array
 // ===============================================================================
 
-var friendData = require("../data/friendData");
+var friendData = require("../Data/friendData");
 
 
 // ===============================================================================
@@ -25,14 +25,11 @@ module.exports = function(app) {
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
+  // ...the JSON is pushed to the appropriate JavaScript array.
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
+
       friendData.push(req.body);
       res.json(true);
 
--- app/Routing/htmlroutes.js
+++ app/Routing/htmlroutes.js
@@ -16,11 +16,11 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    res.sendFile(path.join(__dirname, "/../Public/survey.html"));
   });
 
   // If no matching route is found default to home
   app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
+    res.sendFile(path.join(__dirname, "/../Public/home.html"));
   });
 };
